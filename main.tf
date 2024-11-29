0949a9332b7cecd9de97d9e4fd11184dec291e29
vasanthvkjc <59336263+vasanthvkjc@users.noreply.github.com>
2024-11-29 12:47:57 +0000

    Added autoscaling module

--- main.tf
+++ main.tf
@@ -47,16 +47,20 @@ module "blog_sg" {
   vpc_id = module.blog_vpc.vpc_id
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [ module.blog_sg.security_group_id ]
+
+  image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
 module "blog_alb" {
@@ -77,12 +81,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = [
-        {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      ]
     }
   ]
 
